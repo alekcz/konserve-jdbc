cb1b5af62d59092a59cf02666fd00f9757ebe9e4
alekcz <alekcz@gmail.com>
2022-10-01 09:52:09 +0200

    update postgres and h2 drivers

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.h2database/h2 "1.4.200"]
+                 [com.h2database/h2 "2.1.214"]
                  [seancorfield/next.jdbc "1.2.659"]
-                 [org.apache.derby/derby "10.14.2.0"]
+                 [org.apache.derby/derby "10.16.1.1"]
                  [mysql/mysql-connector-java "8.0.20"]
-                 [org.postgresql/postgresql "42.2.12"]
-                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [org.postgresql/postgresql "42.5.0"]
+                 [org.xerial/sqlite-jdbc "3.39.3.0"]
                  [io.replikativ/konserve "0.6.0-alpha3"]
                  [com.microsoft.sqlserver/mssql-jdbc "9.1.1.jre8-preview"]
                  [com.mchange/c3p0 "0.9.5.5"]]
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 docker-compose up -d
+rm -rf ./temp
+rm -rf ./temph2
+lein clean
 lein cloverage
 #lein test :only konserve-jdbc.core-postgres-test
 docker-compose down -v
